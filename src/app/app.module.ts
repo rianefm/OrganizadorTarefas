63d499e371bb6af5314b11641bddbb5cf05ef285
Riane <rianefmz@gmail.com>
2024-10-20 21:02:46 -0300

    Api google para autetificacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+// Firebase Imports
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from '../environments/environment'; // Importe o seu ambiente com as configurações do Firebase
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig), // Inicializa Firebase
+    AngularFireAuthModule // Módulo de Autenticação do Firebase
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth'; // Importa o AngularFireAuth
+import firebase from 'firebase/compat/app'; // Importa o Firebase
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth, private router: Router) { } // Injeta o serviço de autenticação e o roteador
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  // Método para fazer login com Google
+  loginWithGoogle() {
+    this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then((result) => {
+        console.log('Login bem-sucedido:', result);
+        this.router.navigate(['/home']); // Redireciona para a página inicial após o login
+      })
+      .catch((error) => {
+        console.error('Erro no login:', error);
+      });
   }
 
 }
