8c175a6fadc2c3639b4bb71544d87d9903dea5bc
Riane <rianefmz@gmail.com>
2024-12-02 13:30:22 -0300

    exemplo de como usar a api

---
+++ src/environments/environments.exemplo.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: "YOUR_API_KEY_HERE", // Substitua por sua chave de API do Firebase
+      authDomain: "your-project.firebaseapp.com", // Substitua pelo seu authDomain
+      projectId: "your-project-id", // Substitua pelo ID do projeto
+      storageBucket: "your-project.appspot.com", // Substitua pelo storageBucket
+      messagingSenderId: "your-sender-id", // Substitua pelo senderId
+      appId: "your-app-id", // Substitua pelo App ID
+      measurementId: "your-measurement-id" // Substitua pelo measurementId (opcional)
+    }
+  };
+  
